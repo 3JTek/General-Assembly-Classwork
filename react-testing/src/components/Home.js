d34fa156c29ae377089abbcb0be86ac916905e0b
Mike Hayden <mike.hayden@ga.co>
2019-02-26 09:46:00 +0000

    Adding react testing

---
+++ react-testing/src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <section className="section">
+      <div className="container">
+        <h1 className="title is-1">Home</h1>
+      </div>
+    </section>
+  )
+}
+
+export default Home
